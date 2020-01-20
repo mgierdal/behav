f17c7bf73f98d45ca9779edd2e857645bc8ece53
Marcin Gierdalski <mgierdal@gmail.com>
2020-01-20 14:35:01 -0500

    cleaned up

--- ymaze.py
+++ ymaze.py
@@ -1,21 +1,10 @@
 #!/usr/bin/env python3
 # coding: utf-8
 
-# In[ ]:
-
-
 import csv
 import sys
 
-# In[ ]:
-
-
-filename = 'ymaze.csv'
-filename = 'becia.csv'
 filename = sys.argv[1]
-
-# In[229]:
-
 
 def is_data_valid(data):
     return len(set(data)) == 3
@@ -27,23 +16,10 @@ def get_alternations(data):
     return [t for t in get_triads(data) if len(set(t)) == 3]
 
 
-# In[230]:
-
-
 with open(filename) as f:
     data = list([row for row in csv.DictReader(f)])
 
 header = list(data[0].keys())
-
-
-# In[231]:
-
-
-data
-
-
-# In[232]:
-
 
 for i,record in enumerate(data):
     if ''.join(data[i].values()) == '': continue
@@ -59,29 +35,13 @@ for i,record in enumerate(data):
     data[i].update({'perc_alt_per_entr':(len(alternations)/len(entries))*100})
 
 
-# In[233]:
-
-
-data
-
-
-# In[234]:
-
-
 outname = filename + '.out.csv'
-
-
-# In[235]:
 
 
 with open(outname, 'w', newline='') as f:
     wr = csv.DictWriter(f,data[0].keys())
     wr.writeheader()
     wr.writerows(data)
-
-
-# In[ ]:
-
 
 
 
