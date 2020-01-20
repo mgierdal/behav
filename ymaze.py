b3767219ea3394d0c694eae6decec48f7bdf69fc
Marcin Gierdalski <mgierdal@gmail.com>
2020-01-20 14:46:11 -0500

    added comments and copyright

--- ymaze.py
+++ ymaze.py
@@ -1,25 +1,52 @@
 #!/usr/bin/env python3
 # coding: utf-8
 
+# Copyright (C) Marcin Gierdalski 2020
+
 import csv
 import sys
 
 filename = sys.argv[1]
 
 def is_data_valid(data):
+    """check if field 'entries' contains only 3 symbols
+
+    arguments:
+    data - entries (string of 3 symbols)
+
+    returns:
+    boolean value
+    """
+    
     return len(set(data)) == 3
 
 def get_triads(data):
+    """return list of all triads
+
+    arguments:
+    data - entries (string of 3 symbols)
+
+    returns:
+    list of strings
+    """
     return [data[i:i+3] for i,e in enumerate(data) if i < (len(data)-2)]
 
 def get_alternations(data):
-    return [t for t in get_triads(data) if len(set(t)) == 3]
+    """return list of all alternations
 
+    arguments:
+    data - entries (string of 3 symbols)
+
+    returns:
+    list of strings
+    """
+
+    return [t for t in get_triads(data) if len(set(t)) == 3]
 
 with open(filename) as f:
     data = list([row for row in csv.DictReader(f)])
 
-header = list(data[0].keys())
+header_orig = list(data[0].keys())
 
 for i,record in enumerate(data):
     if ''.join(data[i].values()) == '': continue
@@ -34,9 +61,7 @@ for i,record in enumerate(data):
     data[i].update({'perc_alt_per_triad':(len(alternations)/len(triads))*100})
     data[i].update({'perc_alt_per_entr':(len(alternations)/len(entries))*100})
 
-
 outname = filename + '.out.csv'
-
 
 with open(outname, 'w', newline='') as f:
     wr = csv.DictWriter(f,data[0].keys())
